8dd6d023bf576d8fc12dbd9d8b9378567ddb86df
Francisco José de las Cabadas Balbuena <francisco.cabadas95@gmail.com>
2020-02-13 10:42:19 +0100

    empezando

---
+++ paginas_usuario/session_inicio.php
@@ -0,0 +1,39 @@
+<?php
+
+//require_once " ";
+
+//if (haySesionIniciada()) redireccionar(" ");
+
+?>
+
+
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+
+<?php
+if (isset($_REQUEST["incorrecto"])) {
+    echo "<p>Usuario o contraseña incorrectos.</p>";
+}
+if (isset($_REQUEST["sesionCerrada"])) {
+    echo "<p>Ha salido correctamente. Su sesión está ahora cerrada.</p>";
+}
+?>
+
+<h1>Iniciar sesión</h1>
+
+<form action="listado-muro.php" method="POST">
+    <label><b>Usuario </b></label><input name="identificador" type="text" placeholder="Usuario"><br />
+    <label><b>Contraseña: </b></label><input type="password" name="contrasenna" placeholder="Contraseña"/><br />
+    <label><b>Recuérdame</b></label><input type="checkbox" name="recuerdame" /><br />
+    <br />
+    <input type="Submit" value="Iniciar sesión" />
+</form>
+
+</body>
+</html>
